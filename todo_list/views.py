917081933f450e41f80542899837a635f60b64ac
Shivanshu <craziest.oliyhan13@gmail.com>
2024-07-31 03:17:42 +0530

    update views.py

--- todo_list/views.py
+++ todo_list/views.py
@@ -18,6 +18,9 @@ load_dotenv()
 @api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated]) 
 def home(request):
+	"""
+	handles the creation and search of new todo through actions
+	"""
 	if request.method == 'POST':
 		action = request.POST.get("action")
 		item = request.POST.get("item")
@@ -44,10 +47,13 @@ def about(request):
 
 
 def delete(request, list_id):
-    task = List.objects.get(pk=list_id)
-    task.delete()
-    messages.success(request, ("Task Has Been Deleted!"))
-    return redirect('home')
+	"""
+	delete a todo from todo list
+	"""
+	task = List.objects.get(pk=list_id)
+	task.delete()
+	messages.success(request, ("Task Has Been Deleted!"))
+	return redirect('home')
 
 
 def cross_off(request, list_id):
@@ -64,6 +70,9 @@ def uncross(request, list_id):
 
 
 def edit(request, list_id):
+	"""
+	edit a todo, redirected to home when successful
+	"""
 	if request.method == 'POST':
 		task = List.objects.get(pk=list_id)
 
@@ -82,35 +91,41 @@ def edit(request, list_id):
 @api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 def register(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-        
-        if not username or not password or not email:
-            return Response({'error': 'All fields are required'}, status=status.HTTP_400_BAD_REQUEST)
-        
-        if User.objects.filter(username=username).exists():
-            return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
-     
-        user = User.objects.create_user(username=username, password=password, email=email)
-        user.save()
-        return redirect("login")
-    return render(request, 'register.html')
+	"""
+	register a new user, redirected to login if user is successfully registered.
+	"""
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		email = request.POST.get('email')
+		
+		if not username or not password or not email:
+			return Response({'error': 'All fields are required'}, status=status.HTTP_400_BAD_REQUEST)
+		
+		if User.objects.filter(username=username).exists():
+			return Response({'error': 'Username already exists'}, status=status.HTTP_400_BAD_REQUEST)
+		
+		user = User.objects.create_user(username=username, password=password, email=email)
+		user.save()
+		return redirect("login")
+	return render(request, 'register.html')
 
 @api_view(['GET', 'POST'])
 @permission_classes([AllowAny]) 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
-    return render(request, 'login.html')
+	"""
+	login to user account, if login successful then redirected to home
+	"""
+	if request.method == 'POST':
+		username = request.data.get('username')
+		password = request.data.get('password')
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+	return render(request, 'login.html')
 
 
 def get_public_holiday(request):
